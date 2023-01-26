3f5c3171c5dd130fa1b6b47a1341d32026247dad
root <gamerlife467@gmail.com>
2023-01-26 08:20:52 -0800

    code2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+nclude<stdio.h>
+
+/**
+ * main - Print alphabet.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char z = 'a';
+while (z <= 'z')
+{
+putchar(z);
+z++;
+}
+putchar('\n');
+return (0);
+}
