bdd6c93b94054cda2701e473c98fadb6dac0a40a
root <gamerlife467@gmail.com>
2023-02-02 14:23:37 -0800

    code10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle in the terminal
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{
+int height, width;
+if (size <= 0)
+_putchar('\n');
+for (height = 0; height < size; height++)
+{
+for (width = size - height - 1; width > 0; width--)
+_putchar(' ');
+for (width = height + 1; width > 0; width--)
+_putchar('#');
+_putchar('\n');
+}
+}
