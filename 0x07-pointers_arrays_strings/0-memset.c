92a22857f5a811174032ee9daf505e66544d548a
root <gamerlife467@gmail.com>
2023-02-13 06:49:30 -0800

    task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,22 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * _memset - fills memory with a constant byte
- * @s: memory space to be filled
- * @b: byte to fill with
- * @n: number of spaces to fill
- * Return: pointer to memory area s
+ * _memset - fills the first n bytes of the memory
+ * area pointed to by s with the constant byte b
+ *
+ * @s: string to change
+ * @b: constant byte
+ * @n: number of b to change
+ *
+ * Return: Pointer to the memeora area s
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
 for (i = 0; i < n; i++)
 {
-*(s + i) = b;
+s[i] = b;
 }
 return (s);
 }
