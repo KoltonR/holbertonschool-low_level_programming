ad100af237b9dcbf350db56f703757848ceca848
root <gamerlife467@gmail.com>
2023-02-13 07:09:12 -0800

    task2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,16 +8,19 @@
  * Return: Pointer fo first char in string if found
  * else return NULL
  */
+
 char *_strchr(char *s, char c)
 {
+
 while (*s != '\0')
 {
 if (*s == c)
 return (s);
 else if (*(s + 1) == c)
-return (s + 1)
+return (s + 1);
 s++;
 }
+
 return (s + 1);  /* return null byte if c not found */
 }
 
