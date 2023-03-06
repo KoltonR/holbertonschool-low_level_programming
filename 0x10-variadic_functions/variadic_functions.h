4fed5591d4ca2ffbf7357367804961b634a08e4f
root <gamerlife467@gmail.com>
2023-03-06 07:37:31 -0800

    task0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,20 +1,13 @@
-#ifndef V_FUNCTIONS
-#define V_FUNCTIONS
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
 
-typedef struct type {
-		char input;
-			void (*formtype)(va_list strings);
-} typecheck_t;
 
-void printchar(va_list strings);
-void printint(va_list strings);
-void printstring(va_list strings);
-void printfloat(va_list strings);
+
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-#endif
+
+
+#endif /* _VARIADIC_FUNCTIONS_H_ */
