6db851c24ce1719b53c041e61ec5ac62768f4262
root <gamerlife467@gmail.com>
2023-03-22 16:04:33 -0700

    task0

--- 0x18-doubly_linked_lists/lists.h
+++ 0x18-doubly_linked_lists/lists.h
@@ -5,20 +5,20 @@
 #include <stdlib.h>
 
 /**
- * * struct dlistint_s - doubly linked list
- * * @n: integer
- * * @prev: points to the previous node
- * * @next: points to the next node
- * * description: doubly linked list node structure
- * */
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ * description: doubly linked list node structure
+ */
 
 
 
 typedef struct dlistint_s
 {
-	int n;
-	struct dlistint_s *prev;
-	struct dlistint_s *next;
+int n;
+struct dlistint_s *prev;
+struct dlistint_s *next;
 }
 dlistint_t;
 
