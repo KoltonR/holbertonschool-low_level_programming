8cb323344fa6c8df17fd7f6761453262ad8875d9
root <gamerlife467@gmail.com>
2023-03-22 16:15:03 -0700

    task1

---
+++ 0x18-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns number of nodes
+ * @h: nodes
+ * Return: nodecount
+ *
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+size_t nodeCount = 0;
+const dlistint_t *temp = h;
+
+if (h == NULL)
+return (0);
+
+while (temp != NULL)
+{
+nodeCount++;
+temp = temp->next;
+}
+return (nodeCount);
+}
+
