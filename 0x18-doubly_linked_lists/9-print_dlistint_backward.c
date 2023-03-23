f40f72c720dfb4d0af1d7fe5dd2d78381c9c95f4
root <gamerlife467@gmail.com>
2023-03-23 11:35:46 -0700

    task7

---
+++ 0x18-doubly_linked_lists/9-print_dlistint_backward.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_dlistint_backward - prints dbl lnk lst backwards
+ * @h: pntr to list
+ * Return: number of nodes
+ *
+ */
+
+
+
+size_t print_dlistint_backward(const dlistint_t *h)
+{
+size_t node_count;
+const dlistint_t *temp = h;
+
+if (h == NULL)
+return (0);
+
+while (temp->next)
+temp = temp->next;
+
+for (node_count = 0; temp != NULL; temp = temp->prev)
+{
+printf("%d\n", temp->n);
+node_count++;
+}
+return (node_count);
+}
