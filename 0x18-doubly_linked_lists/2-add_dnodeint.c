74804a19f743d999cfffd422de014a95ce460d70
root <gamerlife467@gmail.com>
2023-03-23 06:17:25 -0700

    task2

--- 0x18-doubly_linked_lists/2-add_dnodeint.c
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -1,25 +1,32 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - adds dbly linked node at begining
- * @head: pntr to list
- * @n: int to add to node
- * Return: new node
+ * add_dnodeint - adds a new node at the beginning
+ * of a doubly linked list
+ * @head: the head of the doubly linked list
+ * @n: the number to place in the new node
+ * return: the new head of the doubly linked list
  */
+
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-        dlistint_t *newnode = malloc(sizeof(dlistint_t));
+dlistint_t *new_node = NULL;
 
-        if (newnode == NULL)
-                return (NULL);
+new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+return (NULL);
 
-        newnode->n = n;
-        newnode->next = *head;
-        newnode->prev = NULL;
-
-        /*head should be NULL first pass through */
-        if (*head != NULL)
-                (*head)->prev = newnode;
-        *head = newnod
-        return (newnode);
+new_node->n = n;
+if (*head)
+{
+new_node->next = *head;
+new_node->prev = (*head)->prev;
+(*head)->prev = new_node;
+*head = new_node;
+return (*head);
+}
+new_node->next = *head;
+new_node->prev = NULL;
+*head = new_node;
+return (*head);
 }
