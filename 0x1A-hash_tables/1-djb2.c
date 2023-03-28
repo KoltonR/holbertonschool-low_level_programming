ab2bd45d94a7b771bef3625a2cc68b04129e5462
root <gamerlife467@gmail.com>
2023-03-27 19:05:01 -0700

    task1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,21 +1,15 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - implementation of the djb2 algorithm
- * @str: string used to generate hash value
- *
- * Return: hash value
+ * key_index - computes the index of a given key
+ * in a hash table
+ * @key: the given key
+ * @size: the size of the hash table
+ * return: the index of a key in a hash table
  */
-unsigned long int hash_djb2(const unsigned char *str)
-{
-unsigned long int hash;
-int c;
 
-hash = 5381;
-while ((c = *str++))
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-}
-return (hash);
+return (hash_djb2(key) % size);
 }
 
