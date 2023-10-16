3b66444da3af9ba3297dda041ec804db2a5fcc1e
Kolton Rhodes <gamerlife467@gmail.com>
2023-10-16 11:10:57 -0500

    redo0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,19 +1,31 @@
-#include "holberton.h"
-/**
- * _puts_recursion - print a string
- * @s:string
- * Return:void
- */
-void _puts_recursion(char *s)
-{
-if (*s)
-{
-	_putchar(*s);
-	_puts_recursion(s + 1);
-}
-else
-{
-	_putchar('\n');
-}
+#include "main.h"
 
+
+/**
+ * _puts_recursion - print string
+ * @s: string to print
+ * Description: print string using recursion
+ * Return: nothing
+ **/
+
+void _puts_recursion(char *s)
+
+
+{
+
+	int i;
+
+	i = 0;
+
+	if (s[i] != '\0')
+
+	{
+		_putchar(s[i]);
+		_puts_recursion(&s[i + 1]);
+	}
+
+	if (s[i] == '\0')
+	{
+	_putchar('\n');
+	}
 }
