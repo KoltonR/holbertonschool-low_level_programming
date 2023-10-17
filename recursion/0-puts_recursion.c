697cab3760bf58ba02a65710bb00c31c220f973e
Kolton Rhodes <gamerlife467@gmail.com>
2023-10-17 16:45:29 -0500

    redo

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
-* _puts_recursion - prints a string
-*
-*@s: char
-*
-*Return: always 0
-*
-*/
+ * _puts_recursion - print a string
+ * @s:string
+ * Return:void
+ */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-		_putchar('\n');
-	else
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
+if (*s)
+{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+else
+{
+	_putchar('\n');
+}
+
 }
